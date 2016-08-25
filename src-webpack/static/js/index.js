92ee214c5ef66f74f0ceb2af484a56c428045e18
qiushaowei <qiushaowei@360.cn>
2016-08-25 15:52:59 +0800

    up

--- src-webpack/static/js/index.js
+++ src-webpack/static/js/index.js
@@ -1,1 +1,14 @@
-console.log( 'index' );
+
+window._page_name = 'index';
+
+var API = require( './include/api.js' )
+    ;
+
+//require( './include/header.js' );
+//require( './include/nav.js' );
+
+//require( './include/user-login.js' );
+//require( './include/search-box.js' );
+
+
+//require( './include/_monitor.js' );
